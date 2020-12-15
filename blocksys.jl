715d7c7547c3e38f9d70201d5f0cda12b94f05e2
Patryk Rygiel <patrygiel99@gmail.com>
2020-12-15 21:43:28 +0100

    Testing notebook and rough gauss impl

--- blocksys.jl
+++ blocksys.jl
@@ -2,11 +2,43 @@
 module blocksys
 
 include("./structs/matrice.jl")
-using .Matrice: SparseMatrice, SideVector
+using .Matrice: SparseMatrice
 
-function gauss_elimination(matrice::SparseMatrice, vector::SideVector, main_element=nothing)
+function gauss_elimination(A, b::Vector, main_element=nothing)
+
+    n = A.size
     
+    # If no main element chosen perform classic algorithm
+    if isnothing(main_element)
+        for k in 1:(n - 1)
+            for i in (k + 1):n
+                
+                m = A.data[i, k] / A.data[k, k]
 
+                for j in (k + 1):n
+                    A.data[i, j] = A.data[i, j] - m * A.data[k, j]
+                end
+
+                b[i] = b[i] - m * b[k]
+            end
+        end
+    end
+
+    # Compute vector x, having an upper triangle matrice
+    x = Vector{Float64}(undef, length(b))
+    x[n] = b[n] / A.data[n, n]
+
+    for k in reverse(1:(n-1))
+        x[k] = b[k]
+
+        for j in (k + 1):n
+            x[k] -= A.data[k, j] * x[j]
+        end
+
+        x[k] /= A.data[k, k]
+    end
+
+    return x
 end
 
 end # End blocksys
--- structs/matrice.jl
+++ structs/matrice.jl
@@ -5,11 +5,6 @@ using SparseArrays
 
 export load_matrice, load_vector
 
-struct SideVector
-    size::Integer
-    data::Vector
-end
-
 struct SparseMatrice
     size::Integer
     sub_size::Integer
@@ -20,22 +15,18 @@ function load_vector(file_path::String)
     open(file_path) do file
         
         size = 0
-        data = []
+        data = Float64[]
 
         for (idx, line) in enumerate(eachline(file))
             
-            # First row in file holds size
-            if idx == 1
-                size = parse(Int, line)
-            
-            # Other rows hold values
-            else
+            # First row in file holds size - omit
+            if idx != 1
                 push!(data, parse(Float64, line))
             end
         end
         
         # Create custom SideVector type
-        vector = SideVector(size, Float64.(vec(data)))
+        vector = vec(data)
         println("Loaded vector with size $size.")
 
         return vector
@@ -49,22 +40,22 @@ function load_matrice(file_path::String)
         sub_size = 0
         
         # Vectors to build a sparse array from
-        I = []
-        J = []
-        V = []
+        I = Int64[]
+        J = Int64[]
+        V = Float64[]
         
         for (idx, line) in enumerate(eachline(file))
             line = split(line)
             
             # First row in file holds size and sub_size
             if idx == 1
-                size = parse(Int, line[1])
-                sub_size = parse(Int ,line[2])
+                size = parse(Int64, line[1])
+                sub_size = parse(Int64,line[2])
             
             # Other rows hold [i, j, value] tuples
             else
-                push!(I, parse(Int, line[1]))
-                push!(J, parse(Int, line[2]))
+                push!(I, parse(Int64, line[1]))
+                push!(J, parse(Int64, line[2]))
                 push!(V, parse(Float64, line[3]))
             end
         end
