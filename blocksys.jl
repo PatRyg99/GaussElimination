e57fa13065c8806ecd64725df4b626c625a7f86d
Patryk Rygiel <patrygiel99@gmail.com>
2020-12-16 14:45:29 +0100

    Some comments

--- blocksys.jl
+++ blocksys.jl
@@ -7,27 +7,28 @@ using .Matrice: SparseMatrice
 function gauss_elimination(A, b::Vector, main_element=nothing)
 
     n = A.size
+    l = A.sub_size
 
     # If no main element chosen perform classic algorithm
     if isnothing(main_element)
         
-        rows = A.sub_size
+        rows = l
 
         # First loop goes through all the rows - O(n)
         for k in 1:(n - 1)
             
-            # Update only up to A.sub_size next rows
+            # Update only up to l next rows
             if k >= rows
-                rows += A.sub_size
+                rows += l
             end
 
-            # Second loop needs to update only up to l rows - O(l)
+            # Second loop needs to update only up to  rows - O(l)
             for i in (k + 1):rows
                 
                 m = A.data[i, k] / A.data[k, k]
 
-                # Update only up to A.sub_size next columns
-                cols = min(rows + A.sub_size, n)
+                # Update only up to l next columns
+                cols = min(rows + l, n)
                 
                 # Third loop needs to update only up to l columns - O(l)
                 for j in (k + 1):cols
@@ -47,7 +48,7 @@ function gauss_elimination(A, b::Vector, main_element=nothing)
     for k in reverse(1:(n-1))
         
         x[k] = b[k]
-        end_col = min(k + A.sub_size, n) 
+        end_col = min(k + l, n) 
 
         # Iterate over columns, only up to l - O(l)
         for j in (k + 1):end_col
