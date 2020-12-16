dcfaf08a55ea7796aa90aabeb88937b2d4240e57
Patryk Rygiel <patrygiel99@gmail.com>
2020-12-16 14:23:14 +0100

    Optimized gauss elimination for specific matrice

--- blocksys.jl
+++ blocksys.jl
@@ -7,15 +7,30 @@ using .Matrice: SparseMatrice
 function gauss_elimination(A, b::Vector, main_element=nothing)
 
     n = A.size
-    
+
     # If no main element chosen perform classic algorithm
     if isnothing(main_element)
+        
+        rows = A.sub_size
+
+        # First loop goes through all the rows - O(n)
         for k in 1:(n - 1)
-            for i in (k + 1):n
+            
+            # Update only up to A.sub_size next rows
+            if k >= rows
+                rows += A.sub_size
+            end
+
+            # Second loop needs to update only up to l rows - O(l)
+            for i in (k + 1):rows
                 
                 m = A.data[i, k] / A.data[k, k]
 
-                for j in (k + 1):n
+                # Update only up to A.sub_size next columns
+                cols = min(rows + A.sub_size, n)
+                
+                # Third loop needs to update only up to l columns - O(l)
+                for j in (k + 1):cols
                     A.data[i, j] = A.data[i, j] - m * A.data[k, j]
                 end
 
@@ -28,10 +43,14 @@ function gauss_elimination(A, b::Vector, main_element=nothing)
     x = Vector{Float64}(undef, length(b))
     x[n] = b[n] / A.data[n, n]
 
+    # Iterate from last row up - O(n)
     for k in reverse(1:(n-1))
+        
         x[k] = b[k]
+        end_col = min(k + A.sub_size, n) 
 
-        for j in (k + 1):n
+        # Iterate over columns, only up to l - O(l)
+        for j in (k + 1):end_col
             x[k] -= A.data[k, j] * x[j]
         end
 
--- structs/matrice.jl
+++ structs/matrice.jl
@@ -5,16 +5,26 @@ using SparseArrays
 
 export load_matrice, load_vector
 
+struct CustomMatrice
+    size::Integer
+    sub_size::Integer
+    A::Array
+    B::Array
+    C::Array
+end
 struct SparseMatrice
     size::Integer
     sub_size::Integer
     data::SparseMatrixCSC
 end
 
+
 function load_vector(file_path::String)
+    """
+    Loads side vector from given file
+    """
     open(file_path) do file
-        
-        size = 0
+    
         data = Float64[]
 
         for (idx, line) in enumerate(eachline(file))
@@ -27,13 +37,16 @@ function load_vector(file_path::String)
         
         # Create custom SideVector type
         vector = vec(data)
-        println("Loaded vector with size $size.")
+        println("Loaded vector with size $(length(vector)).")
 
         return vector
     end
 end
 
 function load_matrice(file_path::String)
+    """
+    Loads matrice from file as Julia SparseMatrixCSC
+    """
     open(file_path) do file
         
         size = 0
@@ -59,9 +72,86 @@ function load_matrice(file_path::String)
                 push!(V, parse(Float64, line[3]))
             end
         end
+
+        # Insert zeros where elements will be inserted
+        # C matrices under diagonal
         
+        # c1
+        # 0 c2
+        # 0 0 c3
+        # 0 0 0 c4
+        
+        for row in 1:size-4
+            if row % sub_size != 1
+
+                start_col = sub_size * (row ÷ (sub_size + 1) + 1) + 1
+                end_col = sub_size + row - 1
+
+                for col in start_col:end_col
+                    push!(I, row)
+                    push!(J, col)
+                    push!(V, 0.0)
+                end
+            end
+        end
+
         # Create custom SparseMatrice type
         mat = SparseMatrice(size, sub_size, sparse(I, J, V))
+        println("Loaded matrice with size $size and sub_size $(sub_size).")
+
+        return mat
+    end
+end
+
+
+function load_custom_matrice(file_path::String)
+    """
+    Loads matrice from file as custom data structure
+    """
+    open(file_path) do file
+        
+        # First row in file holds size and sub_size
+        size, sub_size = split(readline(file))
+        
+        size = parse(Int64, size)
+        sub_size = parse(Int64, sub_size)
+        v = size ÷ sub_size
+
+        # Allocate matrice
+        A = zeros((v, sub_size, sub_size))
+        B = zeros((v-1, sub_size))
+        C = zeros((v-1, sub_size, sub_size))
+
+        # Other rows hold [i, j, value] tuples
+        for (idx, line) in enumerate(eachline(file))
+            
+            if idx != 0
+                line = split(line)
+
+                i = parse(Int64, line[1])
+                j = parse(Int64, line[2])
+                v = parse(Float64, line[3])
+
+                indice = ((i-1) ÷ sub_size) + 1
+                
+                # Filling matrice A when value on diagonal (sub_size, sub_size)
+                if abs(i - j) < sub_size
+                    A[indice, (i-1) % sub_size + 1, (j-1) % sub_size + 1] = v
+                
+                # Filling matrice B when value under diagonal
+                elseif i >= j
+                    B[indice - 1, (j-1) % sub_size + 1] = v
+
+                # Filling matrice C when value above diagonal
+                else
+                    C[indice, (i-1) % sub_size + 1, (i-1) % sub_size + 1] = v
+                end
+
+            end
+        end
+
+        # Create custom Matrice type
+        mat = CustomMatrice(size, sub_size, A, B, C)
         println("Loaded matrice with size $size and sub_size $(sub_size).")
 
         return mat
