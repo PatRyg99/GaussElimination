b510f3ff17fcb78d14151c21a14c4c9c63adfa2e
Patryk Rygiel <patrygiel99@gmail.com>
2020-12-19 17:36:27 +0100

    Added LU decomposition and LU gauss elimination

--- blocksys.jl
+++ blocksys.jl
@@ -1,10 +1,118 @@
 # Patryk Rygiel
 module blocksys
 
-include("./structs/matrice.jl")
-using .Matrice: SparseMatrice
+function LU_decomposition(A; partial_choice::Bool = false)
+    """Decomposition of a Matrice into L and U matrices"""
+
+    n = A.size
+    l = A.sub_size
+
+    rows = l
+
+    # Permutation vector
+    P = collect(1:n)
+
+    # First loop goes through all the rows - O(n)
+    for k in 1:(n - 1)
+
+        # Partial choice of main element
+        if partial_choice
+
+            idx = k
+            max = abs(A.data[k][k - A.shift[k]])
+
+            end_row = k + l - (k % l) - 1
+            
+            # Iterate over rows with non-zeros and check for column max
+            for i in k+1:end_row
+
+                v = abs(A.data[i][k - A.shift[i]])
+
+                if max < v
+                    idx = i
+                    max = v  
+                end
+            end
+            
+            # Swap i-th row with k-th
+            if idx != k
+
+                temp_A = A.data[k]
+                A.data[k] = A.data[idx]
+                A.data[idx] = temp_A
+
+                temp_shift = A.shift[k]
+                A.shift[k] = A.shift[idx]
+                A.shift[idx] = temp_shift
+
+                temp_p = P[k]
+                P[k] = P[idx]
+                P[idx] = temp_p
+            end
+        end
+
+        if k >= rows
+            rows += l
+        end
+        
+        # Elements in column below are being divided by the diagonal element
+        d = A.data[k][k - A.shift[k]]
+
+        # Second loop needs to update only up to l rows - O(l)
+        for i in (k + 1):rows
+            
+            A.data[i][k - A.shift[i]] /= d 
+            cols = min(A.shift[k] + length(A.data[k]), n)
+            
+            # Third loop needs to update only up to 3*l - O(l)
+            # Longest row in array has 3*l columns due to possible swaps
+            for j in (k + 1):cols
+
+                # Apply Schur complement
+                A.data[i][j - A.shift[i]] -= A.data[i][k - A.shift[i]] * A.data[k][j - A.shift[k]]
+            end
+        end
+    end
+
+    return A, P
+end
+
+
+function LU_gauss_elimination(A, P::Vector, b::Vector)
+    """Computing Gauss elimination on LU matrice - saved in A matrice"""
+
+    n = A.size
+    x = Vector{Float64}(undef, length(b))
+
+    # Iterate from first row to apply permutation - O(n)
+    for k in 1:n
+        x[k] = b[P[k]]
+
+        for j in (A.shift[k] + 1):(k-1)
+            x[k] -= A.data[k][j - A.shift[k]] * x[j]
+        end 
+    end
+
+    # Iterate from last row up - O(n)
+    for k in reverse(1:n)
+    
+        end_col = min(A.shift[k] + length(A.data[k]), n)
+
+        # Iterate over columns, only up to 3*l - O(l)
+        # Longest row in array has 3*l columns due to possible swaps
+        for j in (k + 1):end_col
+            x[k] -= A.data[k][j - A.shift[k]] * x[j]
+        end
+
+        x[k] /= A.data[k][k - A.shift[k]]
+    end
+
+    return x
+end
+
 
 function gauss_elimination(A, b::Vector; partial_choice::Bool = false)
+    """Basic Gauss elimination taking into account specific matrice structure"""
 
     n = A.size
     l = A.sub_size
